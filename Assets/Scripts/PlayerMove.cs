83c3fcc18f41067c9c81c80f81340d0126636726
이승현 <seunghyun.lee@brique.co.kr>
2020-09-03 23:31:08 +0900

    Player 에니메이션 추가

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        // Stop Speed
+        if(Input.GetButtonUp("Horizontal")) {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+
+        // Direction Sprite
+        if(Input.GetButtonDown("Horizontal")) {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        // Animation
+        if(Mathf.Abs(rigid.velocity.x) < 0.3) {
+            animator.SetBool("isWalking" , false);
+        } else {
+            animator.SetBool("isWalking" , true);
+        }
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        // Move By Key Control, Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if(rigid.velocity.x > maxSpeed) { // right
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        } else if(rigid.velocity.x < maxSpeed * (-1)) { // left
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+        }
+    }
+}
